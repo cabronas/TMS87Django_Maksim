d2a19c970319c34f39b90cd8de9d3d147eb9d5ac
user <cabronian123@gmail.com>
2023-07-10 20:51:14 +0300

    9.2

--- src/school/models.py
+++ src/school/models.py
@@ -15,3 +15,28 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name + " " + self.lastname
+
+
+class Diary(models.Model):
+    avg_score = models.DecimalField(max_digits=4, decimal_places=2)
+    student = models.OneToOneField(
+        'Student',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='Diary'
+    )
+
+    def __str__(self):
+        return self.student.name + " " + self.student.lastname + " " + str(self.avg_score)
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    page_count = models.IntegerField(max_length=100)
+    students = models.ManyToManyField(
+        'Student',
+        related_name='Books'
+    )
+
+    def __str__(self):
+        return self.name
