43386fe155dda9166f54bd0d82e2840d0f578e6f
user <cabronian123@gmail.com>
2023-07-13 21:58:00 +0300

    9.2

--- src/catdogs/models.py
+++ src/catdogs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class AImage(models.Model):
+    url = models.URLField()
+    kind = models.CharField(default='cat',
+                            max_length=5,
+                            choices=[('cat', 'Cat'), ('dog', 'Dog')])
+    date_of_creation = models.DateField(auto_now_add=True)
+    type = models.CharField(default='png',max_length=5, choices=[('png', 'PNG'), ('gif', 'GIF'), ('jpg', 'JGP'), ('jpeg', 'JPEG')])
