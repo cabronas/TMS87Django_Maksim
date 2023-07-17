5420546cb9c41ab611c5bc67ee560445d2cd3a73
user <cabronian123@gmail.com>
2023-07-17 21:39:58 +0300

    9.2

--- src/catdogs/urls.py
+++ src/catdogs/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from catdogs.views import CD
+from catdogs.views import CD, cdSave
 from school.views import *
 
 urlpatterns = [
     path('', CD , name='CD'),
-    path('cdsave', cdSave, 'cdSave')
+    path('cdsave', cdSave, name='cdSave')
 ]
--- src/catdogs/views.py
+++ src/catdogs/views.py
@@ -9,21 +9,28 @@ def CD(request):
     if request.method == "GET":
         return render(request, 'choice.html')
     else:
-        session = request.session['catdog']
+        session = request.session.set_expiry(30)
         con = {}
         if 'cats' in request.POST:
             pic = requests.get('https://api.thecatapi.com/v1/images/search/').json()
             con = {'url': pic[0]["url"]}
-            # filetype = pic[0]["url"].split('.')[-1]
+            filetype = pic[0]["url"].split('.')[-1]
+            session_data = {"url": pic[0]["url"], "kind": "cat", "type": filetype}
             # AImage.objects.create(url=pic[0]["url"], kind="cat", type=filetype)
         elif "dogs" in request.POST:
             pic = requests.get('https://dog.ceo/api/breeds/image/random').json()
             con = {'url': pic["message"]}
-            # filetype = pic["message"].split('.')[-1]
+            filetype = pic["message"].split('.')[-1]
+            session_data = {"url": pic["message"], "kind": "cat", "type": filetype}
             # AImage.objects.create(url=pic["message"], kind="cat", type=filetype)
+        request.session['data_for_session'] = session_data
         return render(request, 'pet.html', context=con)
 
 
-def saveCD(request):
-    if request.method == "Post":
-        pass
+def cdSave(request):
+    if request.method == "POST":
+        AImage.objects.create(url=request.session['data_for_session']['url'],
+                              kind=request.session['data_for_session']['kind'],
+                              type=request.session['data_for_session']['type'])
+        con = {'url': request.session['data_for_session']['url']}
+    return render(request, 'saved.html', context=con)
