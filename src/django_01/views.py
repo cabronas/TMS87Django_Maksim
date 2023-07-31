0ccbb7198fd41c5a4dab93813c03ab8c63ede386
Zver <cabronian123@gmail.com>
2023-07-31 19:22:16 +0300

    Switching branch

--- src/django_01/views.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-# Create your views here.
-def dt(request):
-    return HttpResponse(datetime.datetime.now())
--- src/django_09_end/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from django_09_end.models import Customer
-
-# Register your models here.
-admin.site.register(Customer)
